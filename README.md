bd748147903066d276d10a0d1b146e82d0ae678f
sergeyshalygin <shalygin.javascript@gmail.com>
2023-12-10 16:06:04 +0300

    feat(step2): добавлен формик, и страница 404

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Shalygin-Sergey/frontend-project-12/workflows/hexlet-check/badge.svg)](https://github.com/Shalygin-Sergey/frontend-project-12/actions)
 
-[Chat-sluck](https://frontend-project-12-production-da79.up.railway.app)
+[Chat-sluck](https://hexlet-chat-7e1y.onrender.com)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<Home />}/>
+        <Route path="/login" element={<Login />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
