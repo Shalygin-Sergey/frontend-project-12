d05190ee83f938115c3712e2b3eafa707090ccf5
sergeyshalygin <shalygin.javascript@gmail.com>
2023-12-11 11:09:37 +0300

    feat(step3): добавил кнопку

--- frontend/src/components/login/LoginForm.js
+++ frontend/src/components/login/LoginForm.js
@@ -1,30 +1,38 @@
 import { Formik, Form, Field } from 'formik';
-import './style.css';
+import Button from "../buttons/Button";
 
 const LoginForm = () => {
 	return (
-		<Formik initialValues={{ email: "", password: "" }} >
+		<Formik initialValues={{ name: "", password: "" }}
+						onSubmit={({ setSubmitting }) => {
+							console.log("Form is validated! Submitting the form...");
+							setSubmitting(false);
+						}}
+		>
 			{() => (
 				<div className="container">
 					<div className="form-card">
 						<Form>
 							<div className="form-group">
-								<label htmlFor="email">Email</label>
+								<label htmlFor="name"></label>
 								<Field
-									type="email"
-									name="email"
+									id="name"
+									type="text"
+									name="name"
 									className="form-control"
 								/>
 							</div>
 							<div className="form-group">
-								<label htmlFor="password">Password</label>
+								<label htmlFor="password"></label>
 								<Field
+									id="password"
 									type="password"
 									name="password"
 									className="form-control"
 								/>
 							</div>
 						</Form>
+						<Button title="Войти" />
 					</div>
 				</div>
 			)}
